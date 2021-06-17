f02653dfc6c7fa05022d0aae2d03748622aa9355
Thomas Luk <wing.chungluk@faber.work>
2021-06-17 14:15:37 +0100

    add more rest endpoint

--- src/main/java/com/ewise/cooking/controller/ItemController.java
+++ src/main/java/com/ewise/cooking/controller/ItemController.java
@@ -1,9 +1,12 @@
 package com.ewise.cooking.controller;
 
 import com.ewise.cooking.rest.RestItem;
+import com.ewise.cooking.service.ItemService;
 import java.time.LocalDate;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemController {
 
-  private static Logger logger = LoggerFactory.getLogger(ItemController.class);
+  private static final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+  @Autowired
+  private ItemService itemService;
 
   @GetMapping("v0/items/{name}")
   public RestItem getItem(@PathVariable String name) {
@@ -19,5 +25,12 @@ public class ItemController {
 
     // test
     return new RestItem(111L, name, 20, LocalDate.of(2021, 2, 12), "Birmingham", "Tesco");
+  }
+
+  @GetMapping("v0/items/{name}")
+  public List<RestItem> getItems() {
+    logger.info("BEGIN getItems");
+
+    return itemService.getItems();
   }
 }
--- src/main/java/com/ewise/cooking/service/ItemService.java
+++ src/main/java/com/ewise/cooking/service/ItemService.java
@@ -3,6 +3,8 @@ package com.ewise.cooking.service;
 import com.ewise.cooking.beans.Item;
 import com.ewise.cooking.dal.ItemDao;
 import com.ewise.cooking.rest.RestItem;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,11 @@ public class ItemService {
 
     return convertToRest(itemDao.createItem(item));
   }
+
+  public List<RestItem> getItems() {
+    return itemDao.getAllItems().stream().map(this::convertToRest).collect(Collectors.toList());
+  }
+
 
   private Item convertToBean(RestItem restItem) {
     return new Item(restItem.getName(), restItem.getPrice(), restItem.getPriceDate(),
